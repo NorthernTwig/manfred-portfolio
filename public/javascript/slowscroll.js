796ee0c8d98b5c1c6c1b7fed46d87a964e2ca82a
Oscar Nordquist <on222bx@student.lnu.se>
2016-07-28 11:17:41 +0200

    more stuff happend

--- public/javascript/slowscroll.js
+++ public/javascript/slowscroll.js
@@ -1,6 +1,47 @@
 "use strict";
 
-// Where are we?
-// Where is the tag?
-// Distance to tag (Subtract or add depending)
-// Take window top and use the information above
+let currentPosition = () => {
+  // Where are we?
+
+  return window.pageYOffset;
+
+}
+
+let anchorPosition = () => {
+  // Where is the anchor?
+
+  let anchor = document.querySelector(".manfred-info").offsetTop;
+  return anchor;
+
+}
+
+let distanceToScroll = () => {
+  // Distance to tag (Subtract or add depending)
+
+  if (currentPosition() > anchorPosition()) {
+    return currentPosition() - anchorPosition();
+  } else {
+    return anchorPosition() - currentPosition();
+  }
+
+}
+
+let performScrollAction = (e) => {
+  // Take window top and use the information above
+
+  console.log(distanceToScroll());
+  e.target.preventDefault();
+  window.scrollTo(0, distanceToScroll());
+
+}
+
+let anchorLink = () => {
+  let navbar = document.querySelectorAll(".nav-bar a");
+
+  for (let i = 0;i < navbar.length; i++) {
+    navbar[i].addEventListener("click", performScrollAction, false);
+  }
+
+}
+
+anchorLink();
