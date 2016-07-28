75bccb45d6259897482483b115a1cdd5d55f4c64
NorthernTwig <on222bx@student.lnu.se>
2016-07-28 11:21:36 +0200

    Not supposed to be here
    Changed tokens

--- config/secret/vimeo-tokens.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = {
-  "CLIENT_ID": "02022e585e71246322fa07cf859d4af80fbf2aea",
-  "CLIENT_SECRET": "HPqeVmxqB9v2tp6T6sz0ed3B80Cy8NgWAF10sQowfPTMKMyjAgY63nxjjiAi6NhkyQMJS/vhN1r7wyKfzVi+m5rEnOZb23mpk0AdyUxtt2Oe18DsavDKM7nM1mSw9SCm",
-  "ACCESS_TOKEN": "7f9105ad5a50b511c483592a65991b85"
-}
