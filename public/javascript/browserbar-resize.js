219fb3191183f3a47fd269594a2beca2efdb11bb
Oscar Nordquist <on222bx@student.lnu.se>
2016-09-21 09:10:01 +0200

    testing

--- public/javascript/browserbar-resize.js
+++ public/javascript/browserbar-resize.js
@@ -3,16 +3,18 @@
 const selector = () => {
   let header = document.querySelector("header");
   resizer(header);
-  window.addEventListener("orientationchange", resizer.bind(null, header), true);
+  window.addEventListener("resize", resizer.bind(null, header));
 }
 
 const getHeight = (header) => {
-  let currentHeight = window.getComputedStyle(header).getPropertyValue("webkitLogicalHeight")
+  let currentHeight = window.getComputedStyle(header).getPropertyValue("height");
+  // console.log(window.getComputedStyle(header).getPropertyValue("height"));
   return currentHeight;
 }
 
 const resizer = (header) => {
   let height = getHeight(header);
+  // console.log(height);
   header.style.height = height + "px";
   header.firstElementChild.style.height = height + "px";
 }
