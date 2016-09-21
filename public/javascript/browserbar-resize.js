00ffe6a944d4af80a7d5a171d334fb567bf2a522
Oscar Nordquist <on222bx@student.lnu.se>
2016-09-21 14:21:20 +0200

    Testing

--- public/javascript/browserbar-resize.js
+++ public/javascript/browserbar-resize.js
@@ -1,23 +1,43 @@
 "use strict";
 
-const selector = () => {
+// const selector = () => {
+  // let header = document.querySelector("header");
+  // getHeight(header);
+  // window.addEventListener("resize", getHeight.bind(null, header));
+// }
+
+// const getHeight = (header) => {
+  // setTimeout(function() {
+    // let currentHeight = window.getComputedStyle(header).getPropertyValue("height");
+    // header.style.height = currentHeight;
+    // header.firstElementChild.style.height = currentHeight;
+  // }, 100);
+// }
+
+// const resizer = (header) => {
+//     let height = getHeight(header);
+//     header.style.height = height;
+//     header.firstElementChild.style.height = height;
+//     console.log(header);
+// }
+
+const checkHeight = (header) => {
+  // let header = document.querySelector("header");
+  let currentHeight = window.getComputedStyle(header).getPropertyValue("height");
+  // console.log(currentHeight);
+  return currentHeight;
+}
+
+
+const test = () => {
+  let testing = "";
   let header = document.querySelector("header");
-  resizer(header);
-  window.addEventListener("orientationchange", resizer.bind(null, header));
+  header.style.height = checkHeight(header);
+  window.addEventListener("orientationchange", function() {
+    testing = checkHeight(header);
+    // currentHeight = window.getComputedStyle(header).getPropertyValue("height");
+    header.style.height = testing;
+  });
 }
 
-const getHeight = (header) => {
-  setTimeout(function() {
-    let currentHeight = window.getComputedStyle(header).getPropertyValue("height");
-    console.log(currentHeight);
-    return currentHeight;
-  }, 100);
-}
-
-const resizer = (header) => {
-    let height = getHeight(header);
-    header.style.height = height;
-    header.firstElementChild.style.height = height;
-}
-
-selector();
+test();
