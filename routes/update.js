5bd73df9b9e6563ea611154a1487ff4f6a6023e3
Oscar Nordquist <on222bx@student.lnu.se>
2016-10-03 18:05:11 +0200

    No curl for axel

--- routes/update.js
+++ routes/update.js
@@ -6,6 +6,14 @@ const StaticText = require("../models/staticText.js");
 router.route( "/update" )
   .post(( req, res ) => {
 
+    if (req.session.user) {
+      if (!req.session.user.loggedIn) {
+        return res.send('Fuck off <3');
+      }
+    } else {
+      res.send('Not even close <3');
+    }
+
     let bigText = req.body.big || "Hello, I am Manfred";
     let smallText = req.body.small || "Welcome to my creative corner";
     let titlePart = req.body.titlePart || 3;
